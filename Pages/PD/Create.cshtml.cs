a5750c6f5db99bfba2e3ab5b0492f200604fbd3f
balbachl <lisajbalbach@gmail.com>
2024-04-13 16:08:16 -0400

    Add project files.

---
+++ Pages/PD/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PDApp.Data;
+using PDApp.Models;
+
+namespace PDApp.Pages.PD
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PDApp.Data.PDAppContext _context;
+
+        public CreateModel(PDApp.Data.PDAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Resources Resources { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Resources.Add(Resources);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
